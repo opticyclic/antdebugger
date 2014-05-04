666b8e420da7cb290ebd60d0d1060834f62e6cf8
opticyclic <opticyclic@gmail.com>
2014-05-04 17:45:35 -0400

    Create run config from context

--- src/com/handyedit/ant/run/AntRunConfigurationProducer.java
+++ src/com/handyedit/ant/run/AntRunConfigurationProducer.java
@@ -1,8 +1,14 @@
 package com.handyedit.ant.run;
 
+import com.handyedit.ant.util.AntUtil;
+import com.intellij.execution.Location;
 import com.intellij.execution.actions.ConfigurationContext;
 import com.intellij.execution.actions.ConfigurationFromContext;
 import com.intellij.execution.actions.RunConfigurationProducer;
+import com.intellij.lang.ant.dom.AntDomElement;
+import com.intellij.lang.ant.dom.AntDomProject;
+import com.intellij.lang.ant.dom.AntDomReferenceBase;
+import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.util.Ref;
 import com.intellij.psi.PsiElement;
 
@@ -14,6 +20,21 @@ public class AntRunConfigurationProducer extends RunConfigurationProducer<AntRun
 
     @Override
     protected boolean setupConfigurationFromContext(AntRunConfiguration configuration, ConfigurationContext context, Ref<PsiElement> sourceElement) {
+        Location location = context.getLocation();
+        PsiElement psiLocation = context.getPsiLocation();
+
+        AntDomElement antDomElement = (AntDomElement) AntDomReferenceBase.toDomElement(psiLocation);
+        AntDomProject antDomProject = antDomElement.getAntProject();
+
+        String target = AntUtil.getTarget(psiLocation);
+
+        configuration.setName(target);
+        configuration.setTargetName(target);
+        configuration.setBuildFile(location.getVirtualFile());
+        Sdk targetJdk = antDomProject.getTargetJdk();
+        if (targetJdk != null) {
+            configuration.setJdkName(targetJdk.getName());
+        }
         return true;
     }
 
